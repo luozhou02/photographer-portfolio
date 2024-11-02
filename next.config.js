34c32f64c1dce78aab2b3d64b02a904eb99308c0
luozhou02 <luozhou519@gmail.com>
2024-11-02 17:35:54 +0800

    Add next.config.js for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true
+    },
+    basePath: '/photographer-portfolio'
+  }
+
+  module.exports = nextConfig
